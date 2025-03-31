31a57cf81b626dfe43843d12c4a9b8d50bde977e
aakalewii <146139742+aakalewii@users.noreply.github.com>
2025-03-31 15:29:03 +0100

    Arreglar

--- src/com/empresa/model/Cliente.java
+++ src/com/empresa/model/Cliente.java
@@ -3,8 +3,7 @@ package com.empresa.model;
 import java.util.UUID;
 
 public class Cliente {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
     String idCliente;
     String nombre;
 
@@ -13,20 +12,5 @@ public class Cliente {
         this.nombre = nombre;
     }
 
-=======
-=======
->>>>>>> Stashed changes
-    String id;
-    String nombre;
-    
-
-    public Cliente(String nombre) {
-        this.id = UUID.randomUUID().toString();
-        this.nombre = nombre;
-    }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
 
